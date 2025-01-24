b972e202cc5ebc3cfc8c4529cfaa52049e524d8b
HemantKumar110501 <hemantkumar110501@gmail.com>
2025-01-24 19:36:05 +0530

    Add files via upload

---
+++ bbms/index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blood Bank Management</title>
+    <!-- Bootstrap files -->
+    <link rel="stylesheet" href="bootstrap/css//bootstrap.min.css">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!-- External CSS file -->
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+        <a class="navbar-brand" href="index.php">Blood Bank Management System</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="admin/login.php">Admin</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="donor/login.php">Donor</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="patient/login.php">Patient</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- Main content -->
+    <div class="container-fluid" style="margin:50px;">
+        <div class="row container" style="text-align: justify;">
+            <div class="col-md-8 mx-auto content-left">
+                <h4>What is Blood Bank Management System?</h4>
+                <p>
+                    The Blood Bank Management System (BBMS) is an website that stores, processes, retrieves, and analyses data about blood bank administration. It also supervises blood inventory management and other blood bank-related activities. The major goal of the blood bank management system is to keep track of blood, donors, blood groups, blood banks, and stock information. It keeps track of all information concerning blood and its stock.
+                </p>
+
+            </div>
+            <div class="col-md-3 content-right">
+                <img class="img-fluid" src="../BBMS/images/image1.jpg" height="auto" width="70%">
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-10 m-auto" style="text-align: justify;">
+                <h4>What is Blood Bank Management System?</h4>
+                <p>
+                    A person who likes to donate blood/want blood gives his entire details by the help of filling the registration form and can create a username with a password by which he can modify his details in his information given before. Blood is a crucial healthcare resource linked to saving patients' lives with accidents, surgeries, bleeding disorders, pregnancy-related problems and other blood related problems.
+                </p>
+                <p>
+                    Blood is an essential life force, constantly flowing and keeping your body working. Blood is mostly fluid but contains cells and proteins that literally make it thicker than water.
+
+                    Blood has four parts: Red blood cells, white blood cells, platelets and plasma. Each part has specific and important tasks, from carrying oxygen to carrying out waste products.
+
+                    It carries oxygen and nutrients throughout your body.
+                    It forms blood clots to manage bleeding.
+                    It protects your body from infections.
+                    It carries waste products.
+                    It regulates your body temperature.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid" style="margin-bottom: 5%;">
+        <div class="row">
+            <div class="col-10  m-auto">
+                <h4>Parts of Blood</h4>
+                <div class="card container">
+                    <h4>Red Blood Cells</h4>
+                    <hr>
+                    <p>Red Blood Cells account for 45% of your blood. They carry oxygen throughout your body. They also help to clear waste from your body. These cells:
+
+                        Get their distinctive color from the protein hemoglobin.</p>
+                </div>
+                <div class="card">
+                    <h4>White Blood Cells</h4>
+                    <hr>
+                    <p>Our WBC's are less than 1% of our blood and is part of our immune system. When invaders like virus or other cancerous cells starts attack, our white blood cells instantly runs to find and destroy them.</p>
+                </div>
+                <div class="card">
+                    <h4>Plasma</h4>
+                    <hr>
+                    <p>Our blood cells and platelets float in your plasma. It is yellowish fluid that accounts for 55% of our blood. Plasma is our blood’s utility player, covering many bases as it works to keep our body functioning.</p>
+                </div>
+                <p> </p>
+                <div class="card">
+                    <h4>Platelets</h4>
+                    <hr>
+                    <p>They are first on the scene when any blood vessels are damaged or is bleeding. It forms blood clot that seals the damaged vessels so we don’t lose large amount of blood. Platelets:
+                        Account 1% of our blood.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 bg-danger footer">
+                Final Year Project 2024
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
